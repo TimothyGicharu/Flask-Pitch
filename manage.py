3daf8dab659e5077efc55437cb65651aff515fdc
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 20:23:55 +0300

    Create database migrations

--- manage.py
+++ manage.py
@@ -1,11 +1,19 @@
-from pitch import create_app
+from pitch import create_app, db
 from flask_script import Manager,Server
+from pitch.models import User, Post
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+migrate = Migrate(app, db)
 
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Post = Post)
 
 if __name__ == '__main__':
     manager.run()
