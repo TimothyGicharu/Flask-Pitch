1eef652f22fc40de4d0168ffbc9aacec78e438ae
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 13:20:05 +0300

    Modify manage.py and create start.sh

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
 from pitch import create_app
+from flask_script import Manager,Server
 
-app = create_app()
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='08a505fc4b7dbb79a7bd93bccd526901'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://timothy:index506119056@localhost/minutepitch'
+
+python3 manage.py server
