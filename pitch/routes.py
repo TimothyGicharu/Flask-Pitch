3a9b487933f46723daac588e53147aae4aa44506
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 09:46:51 +0300

    Fix typo

--- pitch/routes.py
+++ pitch/routes.py
@@ -164,7 +164,7 @@ def delete_pitch(post_id):
     return redirect(url_for('home'))
 
 
-@app.route('/user/<str:username>')
+@app.route('/user/<string:username>')
 def user_pitch(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
