a2a3f7769b8dcbc58ae39ea1bbbf35cc784c2489
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 17:09:05 +0300

    Change Database URI

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config():
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://timothy:index506119056@localhost:5432/minutepitch'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://nzvxtbraakpbzr:0ded620ec8db233868c78c4bf5ea352da37b4e0376bc6a964e29f9bdeb002ae0@ec2-52-207-124-89.compute-1.amazonaws.com:5432/d6riaoth9obntg'
 
     # SECRET_KEY = '08a505fc4b7dbb79a7bd93bccd526901'
 
@@ -12,7 +12,7 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://timothy:index506119056@localhost:5432/minutepitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost:5432/minutepitch'
     DEBUG = True
 
 
