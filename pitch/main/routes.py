d11aaf8303582c85a36e362a97b5a4682ef175be
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 10:28:22 +0300

    Configure files with blueprint

---
+++ pitch/main/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
+@main.route('/home')
+def home():
+    page = request.args.get('page', 1, type=int)
+    pitches = Post.query.order_by(
+        Post.data_posted.desc()).paginate(page=page, per_page=4)
+    return render_template('home.html', pitches=pitches)
+
+
+@main.route('/about')
+def about():
+    title = 'About'
+    return render_template('about.html', title=title)
---
+++ pitch/pitches/routes.py
@@ -0,0 +1,54 @@
+from flask import Blueprint
+
+pitches = Blueprint('pitches', __name__)
+
+@pitches.route('/pitch/new', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, category=form.category.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_pitch.html', title='New Pitch', form=form,legend='Create Pitch')
+
+
+@pitches.route('/pitch/<int:post_id>')
+def pitch(post_id):
+    pitch = Post.query.get_or_404(post_id)
+    return render_template('post.html', title = pitch.title, pitch=pitch)
+
+
+@pitches.route('/pitch/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_pitch(post_id):
+    pitch = Post.query.get_or_404(post_id)
+    if pitch.author != current_user:
+        abort(403)
+    form = PitchForm()
+    if form.validate_on_submit():
+        pitch.title = form.title.data
+        pitch.content = form.content.data
+        pitch.category = form.category.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('pitch', post_id=pitch.id))
+    elif request.method == 'GET':
+        form.title.data = pitch.title
+        form.content.data = pitch.content
+        form.category.data = pitch.category
+    return render_template('create_pitch.html', title='Update Pitch',
+     form=form, legend='Update Post')
+
+@pitches.route('/pitch/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_pitch(post_id):
+    pitch = Post.query.get_or_404(post_id)
+    if pitch.author != current_user:
+        abort(403)
+    db.session.delete(pitch)
+    db.session.commit()
+    flash('Pitch deleted', 'success')
+    return redirect(url_for('home'))
