2acc37a26e0b20cc77a5cf600b9b398710b6afe5
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 13:46:34 +0300

    Edit start.sh

--- config.py
+++ config.py
@@ -3,17 +3,16 @@ import os
 class Config():
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost/minutepitch'
 
     # SECRET_KEY = '08a505fc4b7dbb79a7bd93bccd526901'
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost/minutepitch'
 
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost:5432/minutepitch'
     DEBUG = True
 
 
