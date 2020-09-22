6b0ac22eed3bc699507e5846fd1bf3b68262b4b5
Timothy Munene <munenegicharu@gmail.com>
2020-09-22 16:03:30 +0300

    Edit Database URI

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config():
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost/minutepitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://timothy:index506119056@localhost:5432/minutepitch'
 
     # SECRET_KEY = '08a505fc4b7dbb79a7bd93bccd526901'
 
